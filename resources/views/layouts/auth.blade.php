42771cc9d658703b81cf146b3b23ac027236f559
TiaWidi <tia.septianawidi@gmail.com>
2025-04-24 20:03:45 +0700

    Parrtial layout

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -2,31 +2,21 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <meta name="description" content="Laravel Role-Based Access Control (RBAC)">
-    <meta name="keywords" content="admin, bootstrap, business, corporate, html5, responsive, Projects, rbac">
-    <meta name="author" content="Tia Widi">
-    <meta name="robots" content="noindex, nofollow">
+  @include('partials._meta')
 
-    <title>Laravel RBAC | Sign In</title>
+  <title>Laravel RBAC | Sign In</title>
 
-    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
-    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/fontawesome.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/all.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+  <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+
+  @include('partials._stylesheet_assets')
 </head>
 
 <body class="account-page">
-    <div class="main-wrapper">
-      @yield('content')
-    </div>
+  <div class="main-wrapper">
+    @yield('content')
+  </div>
 
-    <script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}"></script>
-    <script src="{{ asset('assets/js/feather.min.js') }}"></script>
-    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/js/script.js') }}"></script>
+  @include('partials._script_assets')
 </body>
 
 </html>
---
+++ resources/views/partials/_stylesheet_assets.blade.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/fontawesome.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/all.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
