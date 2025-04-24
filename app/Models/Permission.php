f997b8c794f489aecd16fecd0ae7d6dcc49d690b
TiaWidi <tia.septianawidi@gmail.com>
2025-04-24 12:43:06 +0700

    Update Permission Model

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -9,4 +9,38 @@ class Permission extends Model
 {
     /** @use HasFactory<\Database\Factories\PermissionFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'display_name',
+        'description'
+    ];
+
+    /**
+     * Get the roles that have this permission
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'role_permissions');
+    }
+
+    /**
+     * Get the resources this permission applies to.
+     */
+    public function resources()
+    {
+        return $this->belongsToMany(Resource::class, 'permission_resources');
+    }
+
+    /**
+     * Assigne this permission to a resource
+     */
+    public function assignToResource($resource)
+    {
+        if (is_string($resource)) {
+            $resource = Resource::whereName($resource)->firstOrFail();
+        }
+
+        $this->resources()->syncWithoutDetaching([$resource->id]);
+    }
 }
