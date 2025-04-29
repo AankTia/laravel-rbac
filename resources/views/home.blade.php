37fdfc1245c8bdc0bc1a5bba4305ab77ca023d18
TiaWidi <tia.septianawidi@gmail.com>
2025-04-29 19:23:32 +0700

    Slicing dashboard template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,19 +3,15 @@
 @section('title', 'Home')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">{{ __('Dashboard') }} /</span></h4>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
+<div class="row">
+    <div class="col-md-12">
+        <div class="card mb-4">
+            <h5 class="card-header">{{ __('Dashboard') }}</h5>
+            <!-- Account -->
+            <div class="card-body">
+                <div class="d-flex align-items-start align-items-sm-center gap-4">
                     {{ __('You are logged in!') }}
                 </div>
             </div>
