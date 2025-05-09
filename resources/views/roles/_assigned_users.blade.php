4938593006f1ea3b07c9f6d4385925edbff0c403
TiaWidi <tia.septianawidi@gmail.com>
2025-05-09 20:09:20 +0700

    Partials for assigned role users & role permissions

---
+++ resources/views/roles/_assigned_users.blade.php
@@ -0,0 +1,55 @@
+<div class="card shadow-sm mb-4">
+    <div class="card-header">
+        <h5 class="card-title m-0 me-2">Assigned Users</h5>
+    </div>
+
+    <div class="card-body">
+        <hr class="mt-0">
+        <div class="row">
+            <div class="mb-2">
+                <p>This role is currently assigned to <strong>{{ $role->getTotalUsers() }} users</strong> in the system.</p>
+            </div>
+
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <thead class="table-light">
+                        <tr>
+                            <th>User</th>
+                            <th class="text-center">Assigned At</th>
+                            <th class="text-center">Assigned By</th>
+                            <th class="text-center">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($role->roleUsers as $roleUser)
+                        <tr>
+                            <td class="fw-medium">
+                                <div class="user-info">
+                                    <div class="user-avatar">
+                                        {{ $roleUser->user->initialName() }}
+                                    </div>
+                                    <div class="user-details">
+                                        <p class="user-name"><a href="{{ route('users.show', $roleUser->user) }}">{{ $roleUser->user->name }}</a></p>
+                                        <p class="user-email">{{ $roleUser->user->email }}</p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="fw-medium text-center" nowrap>{{ $roleUser->getFormatedAssignedAt() }}</td>
+                            <td class="fw-medium text-center" nowrap>{{ $roleUser->getAssignedByName() }}</td>
+                            <td class="fw-medium text-center" nowrap>
+                                <button type="button" class="btn btn-sm btn-icon btn-outline-danger">
+                                    <span class="tf-icons {{ deleteIcon() }}"></span>
+                                </button>
+                            </td>
+                        </tr>
+                        @empty
+                        <tr>
+                            <td colspan="3" class="text-center"> No data to show</td>
+                        </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/roles/_role_permissions.blade.php
@@ -0,0 +1,64 @@
+<div class="card shadow-sm mb-4">
+    <div class="card-header d-flex align-items-center justify-content-between">
+        <h5 class="card-title m-0 me-2">Allowed Permissions</h5>
+        @if(auth()->user()->hasPermission('update-role-permissions', 'role'))
+        {!! editButton(route('roles.edit-permissions', $role), 'Update Permissions') !!}
+        @endif
+    </div>
+
+    <div class="card-body">
+        <hr class="mt-0">
+        <div class="row">
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <thead class="table-light">
+                        <tr>
+                            <th>Module</th>
+                            <th class="text-center">Read</th>
+                            <th class="text-center">Create</th>
+                            <th class="text-center">Update</th>
+                            <th class="text-center">Delete</th>
+                            <th class="text-center">Special Privileges</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($modulePermissions as $moduleName => $permissions)
+                        <tr>
+                            <td class="fw-medium">{{ $moduleName }}</td>
+                            <td class="text-center">
+                                @if ($permissions['read'])
+                                <i class="bx bxs-check-circle text-success"></i>
+                                @endif
+                            </td>
+                            <td class="text-center">
+                                @if ($permissions['create'])
+                                <i class="bx bxs-check-circle text-success"></i>
+                                @endif
+                            </td>
+                            <td class="text-center">
+                                @if ($permissions['update'])
+                                <i class="bx bxs-check-circle text-success"></i>
+                                @endif
+                            </td>
+
+                            <td class="text-center">
+                                @if ($permissions['delete'])
+                                <i class="bx bxs-check-circle text-success"></i>
+                                @endif
+                            </td>
+                            <td nowrap>
+                                @foreach ($permissions['others'] as $otherPermission)
+                                <div>
+                                    <i class="bx bxs-check-circle text-success"></i> {{ $otherPermission }}
+                                </div>
+                                @endforeach
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    </div>
+</div>
