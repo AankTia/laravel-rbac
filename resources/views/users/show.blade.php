48711197bc8ecd646373804aa1bae7d8411dac98
TiaWidi <tia.septianawidi@gmail.com>
2025-05-10 12:02:49 +0700

    Timeline style

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -28,20 +28,6 @@
 
 @section('content')
 <style>
-    .profile-image {
-        width: 100px;
-        height: 100px;
-        border-radius: 10px;
-        overflow: hidden;
-        margin-right: 20px;
-        background-color: #c2beff;
-    }
-
-    .profile-image img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
 </style>
 
 <div class="row">
@@ -117,10 +103,93 @@
                 </div>
             </div>
         </div>
+
+        <div class="card mb-6">
+            <div class="card-header">
+                <h5 class="card-title m-0 me-2 mb-2">Activity Timeline</h5>
+                <hr>
+            </div>
+            <div class="card-body pt-3">
+                <ul class="timeline mb-0">
+                    <li class="timeline-item timeline-item-transparent">
+                        <span class="timeline-point timeline-point-primary"></span>
+                        <div class="timeline-event">
+                            <div class="timeline-header mb-3">
+                                <h6 class="mb-0">12 Invoices have been paid</h6>
+                                <small class="text-muted">12 min ago</small>
+                            </div>
+                            <p class="mb-2">
+                                Invoices have been paid to the company
+                            </p>
+                            <div class="d-flex align-items-center mb-2">
+                                <div class="badge bg-lighter rounded d-flex align-items-center">
+                                    <img src="https://demos.themeselection.com/sneat-bootstrap-html-laravel-admin-template/demo/assets/img/icons/misc/pdf.png" alt="img" width="15" class="me-2">
+                                    <span class="h6 mb-0 text-body">invoices.pdf</span>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="timeline-item timeline-item-transparent">
+                        <span class="timeline-point timeline-point-success"></span>
+                        <div class="timeline-event">
+                            <div class="timeline-header mb-3">
+                                <h6 class="mb-0">Client Meeting</h6>
+                                <small class="text-muted">45 min ago</small>
+                            </div>
+                            <p class="mb-2">
+                                Project meeting with john @10:15am
+                            </p>
+                            <div class="d-flex justify-content-between flex-wrap gap-2 mb-2">
+                                <div class="d-flex flex-wrap align-items-center mb-50">
+                                    <div class="avatar avatar-sm me-3">
+                                        <img src="https://demos.themeselection.com/sneat-bootstrap-html-laravel-admin-template/demo/assets/img/avatars/1.png" alt="Avatar" class="rounded-circle">
+                                    </div>
+                                    <div>
+                                        <p class="mb-0 small fw-medium">Lester McCarthy (Client)</p>
+                                        <small>CEO of ThemeSelection</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="timeline-item timeline-item-transparent">
+                        <span class="timeline-point timeline-point-info"></span>
+                        <div class="timeline-event">
+                            <div class="timeline-header mb-3">
+                                <h6 class="mb-0">Create a new project for client</h6>
+                                <small class="text-muted">2 Day Ago</small>
+                            </div>
+                            <p class="mb-2">
+                                6 team members in a project
+                            </p>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap border-top-0 p-0">
+                                    <div class="d-flex flex-wrap align-items-center">
+                                        <ul class="list-unstyled users-list d-flex align-items-center avatar-group m-0 me-2">
+                                            <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar pull-up" aria-label="Vinnie Mostowy" data-bs-original-title="Vinnie Mostowy">
+                                                <img class="rounded-circle" src="https://demos.themeselection.com/sneat-bootstrap-html-laravel-admin-template/demo/assets/img/avatars/1.png" alt="Avatar">
+                                            </li>
+                                            <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar pull-up" aria-label="Allen Rieske" data-bs-original-title="Allen Rieske">
+                                                <img class="rounded-circle" src="https://demos.themeselection.com/sneat-bootstrap-html-laravel-admin-template/demo/assets/img/avatars/4.png" alt="Avatar">
+                                            </li>
+                                            <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar pull-up" aria-label="Julee Rossignol" data-bs-original-title="Julee Rossignol">
+                                                <img class="rounded-circle" src="https://demos.themeselection.com/sneat-bootstrap-html-laravel-admin-template/demo/assets/img/avatars/2.png" alt="Avatar">
+                                            </li>
+                                            <li class="avatar">
+                                                <span class="avatar-initial rounded-circle pull-up text-heading" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-original-title="3 more">+3</span>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 
     <div class="col-md-4">
-
         @if ($user->creatorName() || $user->createdAt())
         <div class="card mb-4">
             <div class="card-body">
